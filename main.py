8b1076cb68661ed71a9023f281e3c51ab9fef0a2
OsamaIssa95 <osama.issa.093@gmail.com>
2025-10-15 19:58:57 +0300

    --add training.py --run mlflow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlflow import MlflowClient
 from pprint import pprint
 from mlflow.entities import experiment
+from training_code import data_val,prepare_data,train_model,params
 from data_generator import generate_apple_sales_data_with_promo_adjustment
 import mlflow
 import pandas as pd
@@ -8,6 +9,8 @@ import dotenv
 import os
 
 dotenv.load_dotenv()
+
+
 #using client api
 """ client = MlflowClient(tracking_uri=os.getenv("MLFLOW-TRACKING-URI"))
 all_experiments = client.search_experiments()
@@ -17,18 +20,34 @@ default_experiment = [
     if experiment.name == "Default"
 ][0]
 pprint(default_experiment) """
+
+
+
+
 #using fluent api
 mlflow.set_tracking_uri(os.getenv("MLFLOW-TRACKING-URI"))
 apple_experiment = mlflow.set_experiment(os.getenv("MLFLOW-EXPERIMENT-NAME"))
 run_name = os.getenv("MLFLOW-RUN-NAME")
 artifact_path = os.getenv("MLFLOW-ARTIFACT-PATH")
-
 data_path = "./data/apple_sales_data.csv"   
-try:
-    df = pd.read_csv(data_path)
-except Exception as e:
-    print(f"{e},generating data...")
-    df = generate_apple_sales_data_with_promo_adjustment()
-    df.to_csv(data_path)
-    print("Data was generated succesfully")
-    
+
+
+def main():
+    try:
+        df = pd.read_csv(data_path)
+    except Exception as e:
+        print(f"{e},generating data...")
+        df = generate_apple_sales_data_with_promo_adjustment()
+        df.to_csv(data_path)
+        print("Data was generated succesfully")
+    x_train, x_val, y_train, y_val = prepare_data(df)
+    rf = train_model(x_train=x_train, y_train=y_train, params=params)
+    metrics = data_val(model=rf, x_val=x_val, y_val=y_val)
+    with mlflow.start_run(run_name=run_name) as run:
+        mlflow.log_params(params)
+        mlflow.log_metrics(metrics)
+        mlflow.sklearn.log_model(sk_model=rf, input_example=x_val, artifact_path=artifact_path)
+
+
+if __name__ == "__main__":
+    main()
--- training_code.py
+++ training_code.py
@@ -1,5 +1,44 @@
+from random import Random
+from joblib.pool import np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error,r2_score
+import pandas as pd
+import numpy as np
+
+params = {
+    "n_estimators": 100,
+    "max_depth": 6,
+    "min_samples_split": 10,
+    "min_samples_leaf": 4,
+    "bootstrap": True,
+    "oob_score": False,
+    "random_state": 888,
+}
 
 
+def prepare_data(data: pd.DataFrame) -> tuple[pd.DataFrame,  pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    X = data.drop(columns=["date", "demand"])
+    y = data["demand"]
+    return train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+def train_model(x_train: pd.DataFrame, y_train: pd.DataFrame, params: dict) -> RandomForestRegressor:
+    rf = RandomForestRegressor(**params)
+    rf.fit(x_train, y_train)
+    return rf
+
+
+def data_val(model: RandomForestRegressor, x_val: pd.DataFrame, y_val: pd.DataFrame) -> dict:
+    y_pred = model.predict(x_val)
+    mae = mean_absolute_error(y_val, y_pred)
+    mse = mean_squared_error(y_val, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_val, y_pred)
+    metrics = {
+        "mae": mae,
+        "mse": mse,
+        "rmse": rmse,
+        "r2": r2
+    }
+    return metrics
